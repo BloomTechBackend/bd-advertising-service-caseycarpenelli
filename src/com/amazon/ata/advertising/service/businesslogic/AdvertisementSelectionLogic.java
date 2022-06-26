84df9dedd81a0c160843aaa18fabbf18c8a16e4b
casey carpenelli <yoyoman190@hotmail.com>
2022-06-26 10:17:17 -0500

    masterytask3

--- src/com/amazon/ata/advertising/service/businesslogic/AdvertisementSelectionLogic.java
+++ src/com/amazon/ata/advertising/service/businesslogic/AdvertisementSelectionLogic.java
@@ -2,10 +2,7 @@ package com.amazon.ata.advertising.service.businesslogic;
 
 import com.amazon.ata.advertising.service.dao.ReadableDao;
 import com.amazon.ata.advertising.service.dao.TargetingGroupDao;
-import com.amazon.ata.advertising.service.model.AdvertisementContent;
-import com.amazon.ata.advertising.service.model.EmptyGeneratedAdvertisement;
-import com.amazon.ata.advertising.service.model.GeneratedAdvertisement;
-import com.amazon.ata.advertising.service.model.RequestContext;
+import com.amazon.ata.advertising.service.model.*;
 import com.amazon.ata.advertising.service.targeting.TargetingEvaluator;
 import com.amazon.ata.advertising.service.targeting.TargetingGroup;
 
@@ -21,7 +18,7 @@ import javax.inject.Inject;
 /**
  * This class is responsible for picking the advertisement to be rendered.
  */
-public class AdvertisementSelectionLogic {
+public class AdvertisementSelectionLogic implements Comparator<TargetingGroup> {
 
     private static final Logger LOG = LogManager.getLogger(AdvertisementSelectionLogic.class);
 
@@ -68,38 +65,72 @@ public class AdvertisementSelectionLogic {
             return new EmptyGeneratedAdvertisement();
         } else {
             final List<AdvertisementContent> contents = contentDao.get(marketplaceId);
-        if (StringUtils.isEmpty(customerId)) {
-            LOG.warn("the Id cannot be empty");
-        } else {
-            //changed what the get took in because it was actually called contentid compared to my previous which was the customerid
-            if (contents.isEmpty()) {
-                return new EmptyGeneratedAdvertisement();
-            }
-//            String contentId = contents.stream().findFirst().get().getContentId();
-            final List<TargetingGroup> content = targetingGroupDao.get(contents.stream().findFirst().get().getContentId()); // returns list of target stuff
-            //the above method is not returning anything. because of an exception
-            if (content.isEmpty()) {
-                return new EmptyGeneratedAdvertisement();
-            }
-            content.stream().sorted(); // im aware it needs to be by click through rate
+//            if (StringUtils.isEmpty(customerId)) {
+//                LOG.warn("the Id cannot be empty");
+//            } else {
+                //changed what the get took in because it was actually called contentid compared to my previous which was the customerid
+                if (contents.isEmpty()) {
+                    return new EmptyGeneratedAdvertisement();
+                }
+                List<TargetingGroup> content = new ArrayList<>();
+                for (int i = 0; i < contents.size(); i++) {
+//                    content = targetingGroupDao.get(contents.get(i).getContentId());
+                    content.add(targetingGroupDao.get(contents.get(i).getContentId()).get(0));
+                }
 
-            TargetingPredicateResult targetingPredicateResult = new TargetingEvaluator(new RequestContext(customerId, marketplaceId))
-                    .evaluate(content.stream().findAny().get());
-            if (targetingPredicateResult.isTrue()) {
-                AdvertisementContent trueAd = contents.get(random.nextInt(contents.size()));
-                generatedAdvertisement = new GeneratedAdvertisement(trueAd);
-            } else {
-                generatedAdvertisement = new EmptyGeneratedAdvertisement();
-            }
+                for (int i = 0; i < contents.size(); i++) {
+//                    content.add(targetingGroupDao.get(contents.get(i).getContentId()).get(i));
+                    if (content.size() >= 1) {
+                        if (content.get(i).getClickThroughRate() > .90) {
+                            TargetingPredicateResult targetingPredicateResult = new TargetingEvaluator(new RequestContext(customerId, marketplaceId))
+                                    .evaluate(content.get(i));
+                            if (targetingPredicateResult.isTrue()) {
+                                return new GeneratedAdvertisement(contents.get(i));
+                            }
+                        }
+                    }
+                }
+
+//                    TargetingPredicateResult targetingPredicateResult = new TargetingEvaluator(new RequestContext(customerId, marketplaceId))
+//                            .evaluate(finish.stream().findFirst().get());
+//                    if (targetingPredicateResult.isTrue()) {
+//                        AdvertisementContent trueAd = contents.get(random.nextInt(contents.size()));
+//                        generatedAdvertisement = new GeneratedAdvertisement(trueAd);
+//                    } else {
+//                        generatedAdvertisement = new EmptyGeneratedAdvertisement();
+//                    }
+
+                if (CollectionUtils.isNotEmpty(contents)) {
+                    AdvertisementContent randomAdvertisementContent = contents.get(random.nextInt(contents.size()));
+                    generatedAdvertisement = new GeneratedAdvertisement(randomAdvertisementContent);
+                }
+
+            return generatedAdvertisement;
         }
-            if (CollectionUtils.isNotEmpty(contents)) {
-                AdvertisementContent randomAdvertisementContent = contents.get(random.nextInt(contents.size()));
-                generatedAdvertisement = new GeneratedAdvertisement(randomAdvertisementContent);
-            }
-        }
-        return generatedAdvertisement;
     }
-}
+
+    @Override
+    public int compare(TargetingGroup o1, TargetingGroup o2) {
+        if (o1.getClickThroughRate() < o2.getClickThroughRate()) {
+            return 0;
+        }
+        if (o1.getClickThroughRate() > o2.getClickThroughRate()) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public Comparator<TargetingGroup> reversed() {
+        return Comparator.super.reversed();
+    }
+
+    @Override
+    public Comparator<TargetingGroup> thenComparing(Comparator<? super TargetingGroup> other) {
+        return Comparator.super.thenComparing(other);
+    }
+
+    }
 //generate ad activty calls this method.
 // and then we get the content id and use the targeting dao to get a list of targeting groups. //PSSST its really customerID
 // sort by click through rate
